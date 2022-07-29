2181c961d1c98ed69421b7ce89fe02123fdfaaa6
rabbanisan <bani.ramadhan99@gmail.com>
2022-07-29 14:59:29 +0700

    add loop.js

---
+++ loop.js
@@ -0,0 +1,75 @@
+//==LOOP==
+// for *
+// for-in
+// for-of
+// foreach
+// while *
+
+// for (let i = 0; i < 5; i++) {
+// 	console.log("xiirpl1");
+// }
+
+// let nama = "RAIHAN";
+// for (let i = nama.length - 1; i >= 0; i--) {
+// 	console.log(nama[i]);
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+let bintang = "";
+// for (let i = 0; i < 1; i++) {
+// 	for (let j = 0; j < 5; j++) {
+// 		bintang = bintang + "*";
+// 		console.log(bintang);
+// 	}
+// }
+
+// for (let j = 0; j < 5; j++) {
+// 	bintang = bintang + "*";
+// 	console.log(bintang);
+// }
+
+// let kata = "hebat";
+// for (let i = 4; i >= 0; i--) {
+// 	console.log(kata[i]);
+// }
+// console.log(kata[4]); //t
+// console.log(kata[3]); //a
+// console.log(kata[2]); //b
+// console.log(kata[1]); //e
+// console.log(kata[0]); //h
+
+// Buatlah program untuk menghitung jumlah kata yang ada.
+
+/**
+ * "i have a dream" >> 4
+ * "lalala" >> 1
+ * "i love javascript" >> 3
+ */
+
+// let kalimat = "xiirpl3";
+// let spasi = 0;
+// for (let i = 0; i < kalimat.length; i++) {
+// 	if (kalimat[0] != " ") {
+// 		if (kalimat[i] == " ") {
+// 			spasi++;
+// 		}
+// 	}
+// }
+// console.log(`jumlah kata = ${spasi}`);
+// // console.log(kalimat.length);
+
+// let kalimat = "aku"; // String
+// let kata = 0;
+// for (let i = 0; i < kalimat.length; i++) {
+// 	if (kalimat[i] !== " " && kalimat[i + 1] == " ") {
+// 		kata++;
+// 	}
+// }
+// if (kalimat[kalimat.length - 1] != " " && kalimat.length != 0) {
+// 	kata++;
+// }
+// console.log(`JUMLAH KATA ADALAH ${kata}`);
